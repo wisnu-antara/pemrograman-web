ece87c5fac61885b288470318b1b973423170c5e
wisnu-antara <60972609+wisnu-antara@users.noreply.github.com>
2020-04-30 00:01:45 +0800

    part3-4

--- materi/javascript/script.js
+++ materi/javascript/script.js
@@ -1,13 +1,15 @@
 (function () {
 "use strict";
+
 document.addEventListener("DCMContentLoaded", function (){
  var state = document.getElementById("s-state");
 
- document
- .getElementById("cart-hplus");
- .addEventListener("submit", estimateTotal);
+document
+.getElementById("cart-hplus");
+addEventListener("submit", estimateTotal);
 
  var btnEstimate = document.getElementById("btn-estimate");
+ 
  btnEstimate.disabled = true;
 
  state.addEventListener("change", function () {
@@ -19,8 +21,54 @@ document.addEventListener("DCMContentLoaded", function (){
  });
     function estimateTotal(event){
         event.preventDefault();
-        alert("testing");
+        alert("silahkan pilih negara pengirim");
         
+        state.focus();
+        return;
+        }
+    
+
+    var itemBball = document.getElementById("txt-q-bbal").value,
+    itemJersey = document.getElementById("txt-q-jersey"),value,
+    itemPower = document.getElementById("txt-q-power").value,
+    shippingState = state.value,
+    shippingMethod = document.querySelector("name=r_method):checked").value;
+    value;
+
+    var totalQty = itemBball + itemJersey + itemPower,
+    shippingCostPer, shippingCost, taxFactor = 1,
+    estimate, totalItemPrice = 98 * itemBball + 25 * itemJersey +38 * itemPower;
+
+    if (shippingState === "CA") {
+        taxFactor = 1.075;
     }
+
+    switch (shippingMethod) {
+        case "usps":
+            shippingCostPer = 2;
+
+            break;
+            case "ups":
+                shippingCostPer = 3;
+
+
+                break;
+
+        default:
+            shippingCostPer = 0;
+            break;
+    }
+
+    shippingCost = shippingCostPer * totalQty;
+
+    estimate = "$" + (totalItemPrice * taxFactor + shippingCost).toFixed(2);
+
+    document.getElementById("txt-estimate").value = estimate;
+           
+    var results = document.getElementById("results");
+    results.innerHTML = "total items: " + shippingCost.toFixed(2) + "<br";
+
+    results.innerHTML += "Tax: " +((taxFactor - 1 ) *100).toFixed(2) + "% (" + shippingState + ")";
+ 
 });
-}) ();
+})();
